0cb937a05eed3b309e67f332fd48f537fb1d0690
JasminaKramer <jasmina.kramer@hfg.design>
2024-11-15 19:13:03 +0100

    basics funktionieren und erste dinge in mobile css eingefügt

--- script/main.js
+++ script/main.js
@@ -14,54 +14,34 @@ $(document).ready(function () {
                 <div class="card-trivia">
                 ${animal.trivia_german}
                 </div>
-
-       
                 <div class="stat-icon">
                     <img src="images/icons/weight.png" alt="weight" />
                 </div>
                 <div class="stat-value">${animal.max_weight}</div>
-
-
-        
                 <div class="stat-icon">
                     <img src="images/icons/length.png" alt="length" />
                 </div>
                 <div class="stat-value">${animal.max_length}</div>
-
-
-          
                 <div class="stat-icon">
                     <img src="images/icons/age.png" alt="maximum age" />
                 </div>
                 <div class="stat-value">${animal.max_age}</div>
-
-
-       
                 <div class="stat-icon">
                     <img src="images/icons/speed.png" alt="max speed" />
                 </div>
                 <div class="stat-value">${animal.top_speed}</div>
-
-
-    
                 <div class="stat-icon">
                     <img src="images/icons/offspring.png" alt="offspring count per cycle" />
                 </div>
                 <div class="stat-value">${animal.litter_size}</div>
-
-
-             
                 <div class="stat-icon">
                     <img src="images/icons/death.png" alt="casualties per year" />
                 </div>
                 <div class="stat-value">${animal.deaths}</div>
-
             </div>
-
-
         </div>`);
-						
-        $('#wrapper').append(divBox);
+                        
+        $('.card-container').append(divBox);
     });
 });
 
